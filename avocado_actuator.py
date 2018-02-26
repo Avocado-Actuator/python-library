5fee36f13be302066b50ab2f257e7fa88b60a2d0
rystm <rystm@gmail.com>
2018-02-26 12:01:52 -0500

    edited existing variable types

--- avocado_actuator.py
+++ avocado_actuator.py
@@ -1,5 +1,6 @@
 from math import degrees
 from enum import Enum
+import serial
 
 class PosUnit(Enum):
   DEGREES = 1
@@ -13,20 +14,23 @@ class Communicator:
     Provides abstractions for communication with an arbitrary number of actuators.
 
     Attributes:
-      port_num (int): Specifies serial port used for communication
+      port_num (int): Specifies serial port used for communication. On *NIX this looks like
+      		'/dev/ttyACM0', on windows like 'COM3'
       pos_unit (PosUnit): Unit for position
       vel_unit (VelUnit): Unit for velocity
+      ser: The Serial object for serial communication
 
     TODO:
       - Specify types of message (probably just strings but maybe worth discussing)
   """
-  def __init__(self, port_num: int, pos_unit: PosUnit = PosUnit.DEGREES, vel_unit: VelUnit = VelUnit.RPM):
+  def __init__(self, port_num: str, pos_unit: PosUnit = PosUnit.DEGREES, vel_unit: VelUnit = VelUnit.RPM):
     """
       Constructor simply setting attributes
     """
     self.port_num = port_num
     self.pos_unit = pos_unit
     self.vel_unit = vel_unit
+    self.ser = serial.Serial(port_num, timeout=0.01)
 
   def _convert_pos_to_degrees(self, pos: float) -> float:
     """
@@ -42,12 +46,12 @@ class Communicator:
     """
     return pos if self.pos_unit == PosUnit.DEGREES else degrees(pos)
 
-  def rotate_to_position(self, id: int, pos: float) -> bool:
+  def rotate_to_position(self, addr: int, pos: float) -> bool:
     """
-      Rotates actuator given by id to postion given by pos
+      Rotates actuator given by addr to postion given by pos
 
       Args:
-        id (int): Identifier for specific actuator to rotate
+       	addr (int): Identifier for specific actuator to rotate
         pos (float): Position specified in self.pos_unit units
 
       Returns:
@@ -60,19 +64,19 @@ class Communicator:
 
     # currently pretending send_to_mcu accepts strings
     pos_message: str = f'some message here plus insert given pos {pos}'
-    send_to_mcu(id, pos_message)
+    send_to_mcu(addr, pos_message)
 
     # currently pretending read_from_mcu returns strings
     response: str = read_from_mcu()
     # in reality unlikely there will only be two possible messages we care about
     return response == 'some message'
 
-  def rotate_at_velocity(self, id: int, vel: float) -> bool:
+  def rotate_at_velocity(self, addr: int, vel: float) -> bool:
     """
-      Rotates actuator given by id at velocity given by vel
+      Rotates actuator given by addr at velocity given by vel
 
       Args:
-        id (int): Identifier for specific actuator to rotate
+        addr (int): Identifier for specific actuator to rotate
         vel (float): Velocity specified in self.vel_unit units
 
       Returns:
@@ -85,19 +89,19 @@ class Communicator:
 
     # currently pretending send_to_mcu accepts strings
     vel_message: str = f'some message here plus insert given vel {vel}'
-    send_to_mcu(id, vel_message)
+    send_to_mcu(addr, vel_message)
 
     # currently pretending read_from_mcu returns strings
     response: str = read_from_mcu()
     # in reality unlikely there will only be two possible messages we care about
     return response == 'some message'
 
-  def rotate_at_current(self, id: int, cur: float) -> bool:
+  def rotate_at_current(self, addr: int, cur: float) -> bool:
     """
-      Rotates actuator given by id at current given by cur
+      Rotates actuator given by addr at current given by cur
 
       Args:
-        id (int): Identifier for specific actuator to rotate
+        addr (int): Identifier for specific actuator to rotate
         cur (float): Current specified in amperes
 
       Returns:
@@ -107,25 +111,25 @@ class Communicator:
 
     # currently pretending send_to_mcu accepts strings
     cur_message: str = f'some message here plus insert given cur {cur}'
-    send_to_mcu(id, cur_message)
+    send_to_mcu(addr, cur_message)
 
     # currently pretending read_from_mcu returns strings
     response: str = read_from_mcu()
     # in reality unlikely there will only be two possible messages we care about
     return response == 'some message'
 
-  def send_to_mcu(self, id: int, message: any):
+  def send_to_mcu(self, addr: int, message: any):
     """
       Currently unimplemented, purpose is to serialize and send a message to MCU
 
       Args:
-        id (int): Placeholder argument for actuator id
+        addr (int): Placeholder argument for actuator id
         message (any): Placeholder argument/type for user message
 
       Returns:
         Placeholder for now
     """
-    pass
+    cmd: str = str(addr) + message
 
   def read_from_mcu(self):
     """
