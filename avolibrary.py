e58de76b8200c8dd43d33dc10583268ae93940bc
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-04-17 15:13:37 -0500

    Clean up documentation

--- avolibrary.py
+++ avolibrary.py
@@ -13,40 +13,36 @@ class VelUnit(Enum):
 	RPM = 2 # rotations per minute
 
 class Communicator:
-	"""
-		Provides abstractions for communication with an arbitrary number of actuators
+	"""Provides abstractions for communication with an arbitrary number of actuators
 
 		Attributes:
-			port_num (int): Specifies serial port used for communication. On *NIX this
-				looks like '/dev/ttyACM0', on windows like 'COM3'
-			pos_unit (PosUnit): Unit for position
-			vel_unit (VelUnit): Unit for velocity
-			ser (Serial): The Serial object for serial communication (timeout
-				arbitrarily set to 0.01s)
+			port_num: specifies serial port used for communication. On *NIX this
+        looks like '/dev/ttyACM0', on Windows like 'COM3'
+			pos_unit: unit for position
+			vel_unit: unit for velocity
+			ser: Serial object for serial communication (timeout arbitrarily
+        set to 0.01s)
 
 		TODO:
 			- Specify types of message (probably just strings but maybe worth discussing)
 	"""
 	def __init__(self, port_num: str, pos_unit: PosUnit = PosUnit.RADIANS, vel_unit: VelUnit = VelUnit.RPS) -> None:
-		"""
-			Constructor simply setting attributes
-		"""
-		self.port_num = port_num
-		self.pos_unit = pos_unit
-		self.vel_unit = vel_unit
-		self.ser = serial.Serial(port_num, 9600, timeout=0.05)
+		"""Constructor simply setting attributes"""
+		self.port_num: int = port_num
+		self.pos_unit: PosUnit = pos_unit
+		self.vel_unit: VelUnit = vel_unit
+		self.ser: serial.Serial = serial.Serial(port_num, 9600, timeout=0.05)
 
 	def __del__(self) -> None:
 		self.ser.close()
 
 	def _convert_pos_to_radians(self, pos: float) -> float:
-		"""
-			Allow users to specify multiple units but simplify MCU communications by
+		"""Allow users to specify multiple units but simplify MCU communications by
 			supporting a single unit on the MCU and converting user provided units to
 			that unit. Choosing radians based on user specifications.
 
 			Args:
-				pos (float): Position specified in self.pos_unit units
+				position specified in self.pos_unit units
 
 			Returns:
 				float
@@ -54,15 +50,14 @@ class Communicator:
 		return pos if self.pos_unit == PosUnit.RADIANS else radians(pos)
 
 	def rotate_to_position(self, addr: int, pos: float) -> str:
-		"""
-			Rotates actuator given by addr to postion given by pos
+		"""Rotates actuator given by addr to postion given by pos
 
 			Args:
-				addr (int): Identifier for specific actuator to rotate
-				pos (float): Position specified in self.pos_unit units
+				addr: identifier for specific actuator to rotate
+				pos: position specified in self.pos_unit units
 
 			Returns:
-				str: whether or not operation succeeded (may want higher fidelity
+				whether or not operation succeeded (may want higher fidelity
 					response in future)
 
 			TODO:
@@ -79,15 +74,14 @@ class Communicator:
 		return response
 
 	def rotate_at_velocity(self, addr: int, vel: float) -> str:
-		"""
-			Rotates actuator given by addr at velocity given by vel
+		"""Rotates actuator given by addr at velocity given by vel
 
 			Args:
-				addr (int): Identifier for specific actuator to rotate
-				vel (float): Velocity specified in self.vel_unit units
+				addr: identifier for specific actuator to rotate
+				vel: velocity specified in self.vel_unit units
 
 			Returns:
-				str: whether or not operation succeeded (may want higher fidelity
+				whether or not operation succeeded (may want higher fidelity
 					response in future)
 
 			TODO:
@@ -105,15 +99,14 @@ class Communicator:
 		return response
 
 	def rotate_at_current(self, addr: int, cur: float) -> str:
-		"""
-			Rotates actuator given by addr at current given by cur
+		"""Rotates actuator given by addr at current given by cur
 
 			Args:
-				addr (int): Identifier for specific actuator to rotate
-				cur (float): Current specified in amperes
+				addr: identifier for specific actuator to rotate
+				cur: current specified in amperes
 
 			Returns:
-				str: whether or not operation succeeded (may want higher fidelity
+				whether or not operation succeeded (may want higher fidelity
 					response in future)
 		"""
 		# the below is pseudocode and should not be expected to run
@@ -127,54 +120,50 @@ class Communicator:
 		return response
 
 	def get_position(self, addr: int) -> str:
-		"""
-			Returns the current position the actuator is at
+		"""Returns the current position the actuator is at
 
 			Args:
-				addr (int): Identifier for specific actuator
+				addr: identifier for specific actuator
 
 			Returns:
-				str: the response of the mcu
+				the response of the mcu
 		"""
 		self._send_to_mcu(addr, 'getpos')
 		response: str = self._read_from_mcu()
 		return response
 
 	def get_velocity(self, addr: int) -> str:
-		"""
-			Returns the current velocity the actuator is rotating at
+		"""Returns the current velocity the actuator is rotating at
 
 			Args:
-				addr (int): Identifier for specific actuator
+				addr: identifier for specific actuator
 
 			Returns:
-				str: the response of the mcu
+				the response of the mcu
 		"""
 		self._send_to_mcu(addr, 'getvel')
 		response: str = self._read_from_mcu()
 		return response
 
 	def get_current(self, addr: int) -> str:
-		"""
-			Returns the current the actuator is operating at
+		"""Returns the current the actuator is operating at
 
 			Args:
-				addr (int): Identifier for specific actuator
+				addr: Identifier for specific actuator
 
 			Returns:
-				str: the response of the mcu
+				the response of the mcu
 		"""
 		self._send_to_mcu(addr, 'getcur')
 		response: str = self._read_from_mcu()
 		return response
 
 	def _send_to_mcu(self, addr: int, message: str) -> bool:
-		"""
-			Serialize and send a message to MCU
+		"""Serialize and send a message to MCU
 
 			Args:
-				addr (int): Placeholder argument for actuator id
-				message (str): Placeholder argument/type for user message
+				addr: Placeholder argument for actuator id
+				message: Placeholder argument/type for user message
 
 			Returns:
 				true for success (placeholder)
@@ -184,11 +173,10 @@ class Communicator:
 		return True
 
 	def _read_from_mcu(self) -> str:
-		"""
-			Purpose is to receive messages from MCU
+		"""Purpose is to receive messages from MCU
 
 			Returns:
-				str: the contents of the receive buffer
+				the contents of the receive buffer
 		"""
 		return self.ser.readlines()
 
