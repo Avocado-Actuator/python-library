d5176fedf2c9f7a2941a245454a91f9c230b1268
Nathan Shelly <nathanshelly@u.northwestern.edu>
2018-05-02 00:05:27 -0500

    Clean

--- avolibrary.py
+++ avolibrary.py
@@ -17,15 +17,11 @@ class Communicator:
   """Provides abstractions for communication with an arbitrary number of actuators
 
     Attributes:
-      port_num: specifies serial port used for communication. On *NIX this
-        looks like '/dev/ttyACM0', on Windows like 'COM3'
+      port_num: specifies serial port used for communication. On *NIX this looks
+        like '/dev/ttyACM0', on Windows like 'COM3'
       pos_unit: unit for position
       vel_unit: unit for velocity
-      ser: Serial object for serial communication (timeout arbitrarily
-        set to 0.01s)
-
-    TODO:
-      - Specify types of message (probably just strings but maybe worth discussing)
+      ser: Serial object for communication (timeout arbitrarily set to 0.01s)
   """
   def __init__(self, port_num: str, pos_unit: PosUnit = PosUnit.RADIANS, vel_unit: VelUnit = VelUnit.RPS) -> None:
     """Constructor simply setting attributes"""
@@ -38,15 +34,17 @@ class Communicator:
     self.ser.close()
 
   def _convert_pos_to_radians(self, pos: float) -> float:
-    """Allow users to specify multiple units but simplify MCU communications by
+    """Convert a given position to radians if it is not already
+
+      Allows users to specify multiple units but simplify MCU communications by
       supporting a single unit on the MCU and converting user provided units to
       that unit. Choosing radians based on user specifications.
 
       Args:
-        position specified in self.pos_unit units
+        pos: position specified in self.pos_unit units
 
       Returns:
-        float
+        position converted to radians
     """
     return pos if self.pos_unit == PosUnit.RADIANS else radians(pos)
 
@@ -54,140 +52,110 @@ class Communicator:
     """Rotates actuator given by addr to postion given by pos
 
       Args:
-        addr: identifier for specific actuator to rotate
+        addr: id for specific actuator to rotate
         pos: position specified in self.pos_unit units
 
       Returns:
-        whether or not operation succeeded (may want higher fidelity
-          response in future)
+        the response of the mcu
 
       TODO:
         - Allow user to specify timing/speed on rotation?
     """
-    # the below is pseudocode and should not be expected to run
-
-    # currently pretending send_to_mcu accepts strings
-    pos_message: str = f'set pos {pos}'
-    self._send_to_mcu(addr, pos_message)
-    # currently pretending read_from_mcu returns strings
-    response: str = self._read_from_mcu()
-    # in reality unlikely there will only be two possible messages we care about
-    return response
+    self._send_to_mcu(addr, f'set pos {pos}')
+    return self._read_from_mcu()
 
   def rotate_at_velocity(self, addr: int, vel: float) -> str:
     """Rotates actuator given by addr at velocity given by vel
 
       Args:
-        addr: identifier for specific actuator to rotate
+        addr: id for specific actuator to rotate
         vel: velocity specified in self.vel_unit units
 
       Returns:
-        whether or not operation succeeded (may want higher fidelity
-          response in future)
+        the response of the mcu
 
       TODO:
         - Allow user to specify how long they would like rotation to occur for?
     """
-    # the below is pseudocode and should not be expected to run
-
-    # currently pretending send_to_mcu accepts strings
-    # vel_message: str = f'some message here plus insert given vel {vel}'
-    vel_message: str = f'set vel {vel}'
-    self._send_to_mcu(addr, vel_message)
-    # currently pretending read_from_mcu returns strings
-    response: str = self._read_from_mcu()
-    # in reality unlikely there will only be two possible messages we care about
-    return response
+    self._send_to_mcu(addr, f'set vel {vel}')
+    return self._read_from_mcu()
 
   def rotate_at_current(self, addr: int, cur: float) -> str:
     """Rotates actuator given by addr at current given by cur
 
       Args:
-        addr: identifier for specific actuator to rotate
+        addr: id for specific actuator to rotate
         cur: current specified in amperes
 
       Returns:
-        whether or not operation succeeded (may want higher fidelity
-          response in future)
+        the response of the mcu
     """
-    # the below is pseudocode and should not be expected to run
-
-    # currently pretending send_to_mcu accepts strings
-    cur_message: str = f'set cur {cur}'
-    self._send_to_mcu(addr, cur_message)
-    # currently pretending read_from_mcu returns strings
-    response: str = self._read_from_mcu()
-    # in reality unlikely there will only be two possible messages we care about
-    return response
+    self._send_to_mcu(addr, f'set cur {cur}')
+    return self._read_from_mcu()
 
   def get_position(self, addr: int) -> str:
     """Returns the current position the actuator is at
 
       Args:
-        addr: identifier for specific actuator
+        addr: id for specific actuator
 
       Returns:
         the response of the mcu
     """
     self._send_to_mcu(addr, 'get pos')
-    response: str = self._read_from_mcu()
-    return response
+    return self._read_from_mcu()
 
   def get_velocity(self, addr: int) -> str:
     """Returns the current velocity the actuator is rotating at
 
       Args:
-        addr: identifier for specific actuator
+        addr: id for specific actuator
 
       Returns:
         the response of the mcu
     """
     self._send_to_mcu(addr, 'get vel')
-    response: str = self._read_from_mcu()
-    return response
+    return self._read_from_mcu()
 
   def get_current(self, addr: int) -> str:
     """Returns the current the actuator is operating at
 
       Args:
-        addr: Identifier for specific actuator
+        addr: id for specific actuator
 
       Returns:
         the response of the mcu
     """
     self._send_to_mcu(addr, 'get cur')
-    response: str = self._read_from_mcu()
-    return response
+    return self._read_from_mcu()
 
   def get_temperature(self, addr: int) -> str:
     """Returns the temperature the actuator is operating at
 
       Args:
-        addr: identifier for specific actuator
+        addr: id for specific actuator
 
       Returns:
         the response of the mcu
     """
     self._send_to_mcu(addr, 'get tmp')
-    response: str = self._read_from_mcu()
-    return response
+    return self._read_from_mcu()
 
-  def _send_to_mcu(self, addr: int, message: str) -> bool:
+  def _send_to_mcu(self, addr: int, message: str) -> None:
     """Serialize and send a message to MCU
 
       Args:
-        addr: Placeholder argument for actuator id
-        message: Placeholder argument/type for user message
+        addr: actuator id
+        message: user message to send
 
       Returns:
-        true for success (placeholder)
+        true for success
     """
-    cmd: str = f'{addr} {message}{STOP_BYTE}'
+    cmd = f'{addr} {message}{STOP_BYTE}'
     self.ser.write(cmd.encode('utf-8'))
-    return True
 
   def _read_from_mcu(self) -> str:
-    """Purpose is to receive messages from MCU
+    """Receive messages from MCU
 
       Returns:
         the contents of the receive buffer
